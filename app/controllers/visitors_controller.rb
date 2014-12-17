b868e477b7035814d7e4ef9f5e4a6835735aec3b
Michael Pierce <michael@affektive.com>
2014-12-17 15:39:47 -0700

    Working redirect for when parish id is nil.

--- app/controllers/visitors_controller.rb
+++ app/controllers/visitors_controller.rb
@@ -1,2 +1,11 @@
 class VisitorsController < ApplicationController
+  include Devise::Controllers::Helpers
+
+  def index
+    if signed_in?
+      if current_user.parish_id == nil
+        redirect_to(new_parish_path)
+      end
+    end
+  end
 end
